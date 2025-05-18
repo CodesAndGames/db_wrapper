14b44ba4568303d353815d01ca03d05d6a7d2367
JaxDanger <jax@jaxdanger.com>
2025-05-18 17:59:24 -0400

    init

---
+++ config.js
@@ -0,0 +1,8 @@
+// Define database information here.
+global.config = {
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'framework',
+	suppressErrors: true,
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version 'cerulean'
+game 'gta5'
+
+shared_script 'production/config.js'
+server_script	'production/server/server.js'
